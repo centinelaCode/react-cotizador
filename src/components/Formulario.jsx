ade8a499f470d4ddebcf9cb4fa6e6024f1ea962a
centinela1024tb <centinela1024tb@gmail.com>
2023-07-05 18:17:42 -0600

    [ADD] Add performance and UI and UX with useCallback, useMemo and useRef

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -10,7 +10,7 @@ const Formulario = () => {
 
    const handleSubmit = e => {
       e.preventDefault();
-      console.log(data)
+      // console.log(data)
 
       // valida si hay campos vacios
       if(Object.values(data).includes('')){
--- src/components/Resultado.jsx
+++ src/components/Resultado.jsx
@@ -1,16 +1,58 @@
-import useCotizador from "../hooks/useCotizador"
+import { useCallback, useMemo, useRef } from 'react'
+import useCotizador from '../hooks/useCotizador'
+import { MARCAS, PLANES } from '../constants'
 
 
 const Resultado = () => {
 
-   const { resultado } = useCotizador();
+   const { resultado, data } = useCotizador()
+   const { marca, year, plan } = data
+
+   // aplicamos array destructuring y usamos en uno useCallback para evitar render del plan y marca hasta que 
+   // cambia el resultado y en otro use memo son muy similares(este hay que hacer el return explicito) y
+   // usamos un useRef para year (ya que este no tiene una funcion)
+   const [nombreMarca] = useCallback( 
+      MARCAS.filter(m => m.id === Number(marca)), 
+      [resultado]
+   )
+   const [nombrePlan] = useMemo( () =>
+      PLANES.filter(p => p.id === Number(plan)),
+      [resultado]
+   )
+   // usamos hook useRef para evitar el render de year, se accede como yearRef.current
+   const yearRef = useRef(year)
+   
 
    if(resultado === 0) {
       return null
    }
 
    return (
-      <div>{resultado}</div>
+      <div className="bg-gray-100 text-center mt-5 p-5 shadow">
+         <h2 className="text-gray-600 font-black text-3xl">
+            Resumen
+         </h2>
+
+         <p className="my-2">
+            <span className="font-bold">Marca: </span> 
+            {nombreMarca.name}
+         </p>
+         <p className="my-2">
+            <span className="font-bold">Plan: </span> 
+            {nombrePlan.name}
+         </p>
+         <p className="my-2">
+            <span className="font-bold">Year Car: </span> 
+            {yearRef.current}
+         </p>
+
+         <p className="my-2 text-2xl">
+            <span className="font-bold">Total Quote: </span> 
+            {resultado}
+         </p>
+
+
+      </div>
    )
 }
 
